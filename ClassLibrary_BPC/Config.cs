b912a9b1760f69cc3bbd5594ece226d332693294
Sukawit Suwannarom|สุขวิทย์ สุวรรณรมย์ <sukawit.s@bhatarapro.com>
2023-12-11 10:24:42 +0700

    update

--- ClassLibrary_BPC/Config.cs
+++ ClassLibrary_BPC/Config.cs
@@ -9,14 +9,14 @@ namespace ClassLibrary_BPC
     public class Config
     {
 
-        static public string Database = "IHI";
-        static public string Userid = "Test";
+        static public string Database = "HRM";
+        static public string Userid = "sa";
 
-        
+
         //static public string Server = "83.118.28.242";
         //static public string Server = "161.82.218.95";
 
-        static public string Server = "DESKTOP-VIP623A";
+        static public string Server = ".\\SQLEXPRESS";
         static public string Password = "Sql2019*";
         //static public string Server = ".\\SQL2019";
         //static public string Password = "2019";
